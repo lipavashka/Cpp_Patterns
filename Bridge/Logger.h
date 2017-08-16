05d28a00bb59a6d5f14564f7f1090c432a6041da
lipavashka <lipavashka@gmail.com>
2017-08-16 16:49:21 +0300

    ...

---
+++ Bridge/Logger.h
@@ -0,0 +1,45 @@
+// Logger.h - јбстракци€
+#include <string>
+using namespace std;
+
+// ќпережающее объ€вление
+class LoggerImpl;
+
+class Logger
+{
+public:
+	Logger(LoggerImpl* p);
+	virtual ~Logger();
+	virtual void log(string & str) = 0;
+protected:
+	LoggerImpl * pimpl;
+};
+
+class ConsoleLogger : public Logger
+{
+public:
+	ConsoleLogger();
+	~ConsoleLogger();
+	void log(string & str);
+};
+
+class FileLogger : public Logger
+{
+public:
+	FileLogger(string & file_name);
+	~FileLogger();
+	void log(string & str);
+private:
+	string file;
+};
+
+class SocketLogger : public Logger
+{
+public:
+	SocketLogger(string & remote_host, int remote_port);
+	~SocketLogger();
+	void log(string & str);
+private:
+	string host;
+	int    port;
+};
