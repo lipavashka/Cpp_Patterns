f0cf3c7b53379bb26d08eb189e48dd62b486c297
lipavashka <lipavashka@gmail.com>
2017-08-17 17:13:49 +0300

    ...

--- Factory_Method/main.cpp
+++ Factory_Method/main.cpp
@@ -5,9 +5,81 @@
 #include <vector>
 using namespace std;
 
-// Реализация паттерна Factory Method на основе обобщенного конструктора
+//// Реализация паттерна Factory Method на основе обобщенного конструктора
+//
+//enum Warrior_ID { Infantryman_ID = 0, Archer_ID, Horseman_ID };
+//
+//// Иерархия классов игровых персонажей
+//class Warrior
+//{
+//public:
+//	virtual void info() = 0;
+//	virtual ~Warrior() {}
+//	// Параметризированный статический фабричный метод
+//	static Warrior* createWarrior(Warrior_ID id);
+//};
+//
+//class Infantryman : public Warrior
+//{
+//public:
+//	void info() {
+//		cout << "Infantryman" << endl;
+//	}
+//};
+//
+//class Archer : public Warrior
+//{
+//public:
+//	void info() {
+//		cout << "Archer" << endl;
+//	}
+//};
+//
+//class Horseman : public Warrior
+//{
+//public:
+//	void info() {
+//		cout << "Horseman" << endl;
+//	}
+//};
+//
+//
+//// Реализация параметризированного фабричного метода
+//Warrior* Warrior::createWarrior(Warrior_ID id)
+//{
+//	Warrior * p = 0;
+//	switch (id)
+//	{
+//	case Infantryman_ID:
+//		p = new Infantryman();
+//		break;
+//	case Archer_ID:
+//		p = new Archer();
+//		break;
+//	case Horseman_ID:
+//		p = new Horseman();
+//		break;
+//	default:
+//		assert(false);
+//	}
+//	return p;
+//};
+//
+//
+//// Создание объектов при помощи параметризированного фабричного метода
+//int main()
+//{
+//	vector<Warrior*> v;
+//	v.push_back(Warrior::createWarrior(Infantryman_ID));
+//	v.push_back(Warrior::createWarrior(Archer_ID));
+//	v.push_back(Warrior::createWarrior(Horseman_ID));
+//
+//	for (int i = 0; i<v.size(); i++)
+//		v[i]->info();
+//	// ...
+//}
 
-enum Warrior_ID { Infantryman_ID = 0, Archer_ID, Horseman_ID };
+// Классическая реализация паттерна Factory Method
 
 // Иерархия классов игровых персонажей
 class Warrior
@@ -15,8 +87,6 @@ class Warrior
 public:
 	virtual void info() = 0;
 	virtual ~Warrior() {}
-	// Параметризированный статический фабричный метод
-	static Warrior* createWarrior(Warrior_ID id);
 };
 
 class Infantryman : public Warrior
@@ -24,7 +94,7 @@ class Infantryman : public Warrior
 public:
 	void info() {
 		cout << "Infantryman" << endl;
-	}
+	};
 };
 
 class Archer : public Warrior
@@ -32,7 +102,7 @@ class Archer : public Warrior
 public:
 	void info() {
 		cout << "Archer" << endl;
-	}
+	};
 };
 
 class Horseman : public Warrior
@@ -40,39 +110,54 @@ class Horseman : public Warrior
 public:
 	void info() {
 		cout << "Horseman" << endl;
-	}
+	};
 };
 
 
-// Реализация параметризированного фабричного метода
-Warrior* Warrior::createWarrior(Warrior_ID id)
+// Фабрики объектов
+class Factory
 {
-	Warrior * p = 0;
-	switch (id)
-	{
-	case Infantryman_ID:
-		p = new Infantryman();
-		break;
-	case Archer_ID:
-		p = new Archer();
-		break;
-	case Horseman_ID:
-		p = new Horseman();
-		break;
-	default:
-		assert(false);
+public:
+	virtual Warrior* createWarrior() = 0;
+	virtual ~Factory() {}
+};
+
+class InfantryFactory : public Factory
+{
+public:
+	Warrior* createWarrior() {
+		return new Infantryman;
 	}
-	return p;
+};
+
+class ArchersFactory : public Factory
+{
+public:
+	Warrior* createWarrior() {
+		return new Archer;
+	}
+};
+
+class CavalryFactory : public Factory
+{
+public:
+	Warrior* createWarrior() {
+		return new Horseman;
+	}
 };
 
 
-// Создание объектов при помощи параметризированного фабричного метода
+// Создание объектов при помощи фабрик объектов
 int main()
 {
+	InfantryFactory* infantry_factory = new InfantryFactory;
+	ArchersFactory*  archers_factory = new ArchersFactory;
+	CavalryFactory*  cavalry_factory = new CavalryFactory;
+
 	vector<Warrior*> v;
-	v.push_back(Warrior::createWarrior(Infantryman_ID));
-	v.push_back(Warrior::createWarrior(Archer_ID));
-	v.push_back(Warrior::createWarrior(Horseman_ID));
+	v.push_back(infantry_factory->createWarrior());
+	v.push_back(archers_factory->createWarrior());
+	v.push_back(cavalry_factory->createWarrior());
 
 	for (int i = 0; i<v.size(); i++)
 		v[i]->info();
