d4fd6e1ca1607efadfe6beb68d15b9fda169bd05
lipavashka <lipavashka@gmail.com>
2017-08-17 17:47:02 +0300

    ...

--- Prototype/main.cpp
+++ Prototype/main.cpp
@@ -5,57 +5,143 @@
 #include <map>
 using namespace std;
 
-// Идентификаторы всех родов войск
-enum Warrior_ID { Infantryman_ID, Archer_ID, Horseman_ID };
+//// Реализация паттерна Ptototype на основе обобщенного конструктора
+//
+//// Идентификаторы всех родов войск
+//enum Warrior_ID { Infantryman_ID, Archer_ID, Horseman_ID };
+//
+//class Warrior;  // Опережающее объявление
+//typedef map<Warrior_ID, Warrior*> Registry;
+//
+//// Реестр прототипов определен в виде Singleton Мэйерса
+//Registry& getRegistry()
+//{
+//	static Registry _instance;
+//	return _instance;
+//}
+//
+//// Единственное назначение этого класса - помощь в выборе нужного
+//// конструктора при создании прототипов
+//class Dummy { };
+//
+//// Полиморфный базовый класс. Здесь также определен статический 
+//// обобщенный конструктор для создания боевых единиц всех родов войск
+//class Warrior
+//{
+//public:
+//	virtual Warrior* clone() = 0;
+//	virtual void info() = 0;
+//	virtual ~Warrior() {}
+//	// Параметризированный статический метод для создания воинов 
+//	// всех родов войск
+//	static Warrior* createWarrior(Warrior_ID id) {
+//		Registry& r = getRegistry();
+//		if (r.find(id) != r.end())
+//			return r[id]->clone();
+//		return 0;
+//	}
+//protected:
+//	// Добавление прототипа в множество прототипов
+//	static void addPrototype(Warrior_ID id, Warrior * prototype) {
+//		Registry& r = getRegistry();
+//		r[id] = prototype;
+//	}
+//	// Удаление прототипа из множества прототипов
+//	static void removePrototype(Warrior_ID id) {
+//		Registry& r = getRegistry();
+//		r.erase(r.find(id));
+//	}
+//};
+//
+//
+//// В производных классах различных родов войск в виде статических 
+//// членов-данных определяются соответствующие прототипы
+//class Infantryman : public Warrior
+//{
+//public:
+//	Warrior* clone() {
+//		return new Infantryman(*this);
+//	}
+//	void info() {
+//		cout << "Infantryman" << endl;
+//	}
+//private:
+//	Infantryman(Dummy) {
+//		Warrior::addPrototype(Infantryman_ID, this);
+//	}
+//	Infantryman() {}
+//	static Infantryman prototype;
+//};
+//
+//class Archer : public Warrior
+//{
+//public:
+//	Warrior* clone() {
+//		return new Archer(*this);
+//	}
+//	void info() {
+//		cout << "Archer" << endl;
+//	}
+//private:
+//	Archer(Dummy) {
+//		addPrototype(Archer_ID, this);
+//	}
+//	Archer() {}
+//	static Archer prototype;
+//};
+//
+//class Horseman : public Warrior
+//{
+//public:
+//	Warrior* clone() {
+//		return new Horseman(*this);
+//	}
+//	void info() {
+//		cout << "Horseman" << endl;
+//	}
+//private:
+//	Horseman(Dummy) {
+//		addPrototype(Horseman_ID, this);
+//	}
+//	Horseman() {}
+//	static Horseman prototype;
+//};
+//
+//
+//Infantryman Infantryman::prototype = Infantryman(Dummy());
+//Archer Archer::prototype = Archer(Dummy());
+//Horseman Horseman::prototype = Horseman(Dummy());
+//
+//
+//int main()
+//{
+//	vector<Warrior*> v;
+//	v.push_back(Warrior::createWarrior(Infantryman_ID));
+//	v.push_back(Warrior::createWarrior(Archer_ID));
+//	v.push_back(Warrior::createWarrior(Horseman_ID));
+//
+//	for (int i = 0; i<v.size(); i++)
+//		v[i]->info();
+//	// ...
+//}
 
-class Warrior;  // Опережающее объявление
-typedef map<Warrior_ID, Warrior*> Registry;
+// Реализация паттерна Prototype с помощью выделенного класса - фабрики
 
-// Реестр прототипов определен в виде Singleton Мэйерса
-Registry& getRegistry()
-{
-	static Registry _instance;
-	return _instance;
-}
-
-// Единственное назначение этого класса - помощь в выборе нужного
-// конструктора при создании прототипов
-class Dummy { };
-
-// Полиморфный базовый класс. Здесь также определен статический 
-// обобщенный конструктор для создания боевых единиц всех родов войск
+// Иерархия классов игровых персонажей
+// Полиморфный базовый класс
 class Warrior
 {
 public:
 	virtual Warrior* clone() = 0;
 	virtual void info() = 0;
 	virtual ~Warrior() {}
-	// Параметризированный статический метод для создания воинов 
-	// всех родов войск
-	static Warrior* createWarrior(Warrior_ID id) {
-		Registry& r = getRegistry();
-		if (r.find(id) != r.end())
-			return r[id]->clone();
-		return 0;
-	}
-protected:
-	// Добавление прототипа в множество прототипов
-	static void addPrototype(Warrior_ID id, Warrior * prototype) {
-		Registry& r = getRegistry();
-		r[id] = prototype;
-	}
-	// Удаление прототипа из множества прототипов
-	static void removePrototype(Warrior_ID id) {
-		Registry& r = getRegistry();
-		r.erase(r.find(id));
-	}
 };
 
 
-// В производных классах различных родов войск в виде статических 
-// членов-данных определяются соответствующие прототипы
+// Производные классы различных родов войск
 class Infantryman : public Warrior
 {
+	friend class PrototypeFactory;
 public:
 	Warrior* clone() {
 		return new Infantryman(*this);
@@ -64,15 +150,12 @@ public:
 		cout << "Infantryman" << endl;
 	}
 private:
-	Infantryman(Dummy) {
-		Warrior::addPrototype(Infantryman_ID, this);
-	}
 	Infantryman() {}
-	static Infantryman prototype;
 };
 
 class Archer : public Warrior
 {
+	friend class PrototypeFactory;
 public:
 	Warrior* clone() {
 		return new Archer(*this);
@@ -81,15 +164,12 @@ public:
 		cout << "Archer" << endl;
 	}
 private:
-	Archer(Dummy) {
-		addPrototype(Archer_ID, this);
-	}
 	Archer() {}
-	static Archer prototype;
 };
 
 class Horseman : public Warrior
 {
+	friend class PrototypeFactory;
 public:
 	Warrior* clone() {
 		return new Horseman(*this);
@@ -98,25 +178,36 @@ public:
 		cout << "Horseman" << endl;
 	}
 private:
-	Horseman(Dummy) {
-		addPrototype(Horseman_ID, this);
-	}
 	Horseman() {}
-	static Horseman prototype;
 };
 
 
-Infantryman Infantryman::prototype = Infantryman(Dummy());
-Archer Archer::prototype = Archer(Dummy());
-Horseman Horseman::prototype = Horseman(Dummy());
+// Фабрика для создания боевых единиц всех родов войск
+class PrototypeFactory
+{
+public:
+	Warrior* createInfantrman() {
+		static Infantryman prototype;
+		return prototype.clone();
+	}
+	Warrior* createArcher() {
+		static Archer prototype;
+		return prototype.clone();
+	}
+	Warrior* createHorseman() {
+		static Horseman prototype;
+		return prototype.clone();
+	}
+};
 
 
 int main()
 {
+	PrototypeFactory factory;
 	vector<Warrior*> v;
-	v.push_back(Warrior::createWarrior(Infantryman_ID));
-	v.push_back(Warrior::createWarrior(Archer_ID));
-	v.push_back(Warrior::createWarrior(Horseman_ID));
+	v.push_back(factory.createInfantrman());
+	v.push_back(factory.createArcher());
+	v.push_back(factory.createHorseman());
 
 	for (int i = 0; i<v.size(); i++)
 		v[i]->info();
